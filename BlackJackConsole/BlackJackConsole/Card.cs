0cac122a732e670d3e49d6658afbc64c06078ee6
coreyvessar <coreyvessar@E1113-22-1269>
2017-08-25 11:22:46 -0500

    asda

---
+++ BlackJackConsole/BlackJackConsole/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackConsole
+{
+    class Card
+    {
+        //* This class represents a playing card object *//
+
+        private  int _value; //this value can be 1 through 13  ace value begins at 1
+        private  char _suit; // this char represents the suit of the card it can be s (spade) d (diamond) c (club) h (heart)
+
+        /// <summary>
+        /// This constructs card and assigns its value and suit
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="suit"></param>
+        public Card(int value, char suit)
+        {
+            _value = value;
+            _suit = suit;
+
+        }
+        /// <summary>
+        /// This gets the value of the card 1-13
+        /// </summary>
+        /// <returns>The value of the card as an int</returns>
+        public int getValue
+        {
+            get { return _value; }
+        }
+
+        /// <summary>
+        /// This gets the suit of the card s,c,d or h
+        /// </summary>
+        /// <returns></returns>
+        public char getSuit
+        {
+           get { return _suit; }
+        }
+
+
+
+    }
+}
---
+++ BlackJackConsole/BlackJackConsole/Deck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackConsole
+{
+    /// <summary>
+    /// This Class represents a deck of playing cards.  It creates a deck of standard 52 cards.  It can suffle, and deal a single card, and reset the deck. 
+    /// </summary>
+    class Deck
+    {
+        private List<Card> _listOfAvalibleCards;
+        private List<Card> _usedCards;
+
+        /// <summary>
+        /// This constructs the deck of playingcards
+        /// </summary>
+        public Deck()
+        {
+            _listOfAvalibleCards = new List<Card>();
+            _usedCards = new List<Card>();
+
+            //All 13 cards for spade
+            for (int i = 1; i < 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 's'));
+            }
+            //all 13 cards for club
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'c'));
+            }
+            //all 13 cards for hearts
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'h'));
+            }
+            //all 13 cards for dia
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'd'));
+            }
+        }
+        /// <summary>
+        /// This Method deals a card from the top of the deck, removes it from the _listOfAvalibleCards and adds it to the used cards
+        /// </summary>
+        /// <returns></returns>
+        public Card dealCard()
+        {
+            Card dealtCard = _listOfAvalibleCards[0];
+            _listOfAvalibleCards.RemoveAt(0);
+            _usedCards.Add(dealtCard);
+
+            return dealtCard;
+
+        }
+
+            /// <summary>
+            /// This method shuffles the cards in the _listOfAvalibleCards  IT DOES NOT add used cards back to the deck.  Reset method adds the used cards back to the deck then shuffles
+            /// </summary>
+            /// <returns>Returns an exact </returns>
+            public List<Card> Shuffle()
+        {
+            List<Card> shuffledDeck = new List<Card>();
+
+            //not implemented yet
+            return shuffledDeck;
+        }
+
+
+        }
+
+
+    }
+}
