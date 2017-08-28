00839b9d228bb867db64209343885f69b2e0ec66
coreyvessar <coreyvessar@E1113-22-1269>
2017-08-28 10:52:16 -0500

    Finished Card and Deck

--- BlackJackConsole/BlackJackConsole/Deck.cs
+++ BlackJackConsole/BlackJackConsole/Deck.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlackJackConsole
 {
+    
     /// <summary>
     /// This Class represents a deck of playing cards.  It creates a deck of standard 52 cards.  It can suffle, and deal a single card, and reset the deck. 
     /// </summary>
@@ -13,6 +14,8 @@ namespace BlackJackConsole
     {
         private List<Card> _listOfAvalibleCards;
         private List<Card> _usedCards;
+
+        
 
         /// <summary>
         /// This constructs the deck of playingcards
@@ -61,16 +64,68 @@ namespace BlackJackConsole
             /// This method shuffles the cards in the _listOfAvalibleCards  IT DOES NOT add used cards back to the deck.  Reset method adds the used cards back to the deck then shuffles
             /// </summary>
             /// <returns>Returns an exact </returns>
-            public List<Card> Shuffle()
+            public void Shuffle()
         {
-            List<Card> shuffledDeck = new List<Card>();
+            _listOfAvalibleCards.Shuffle();
+        }
 
-            //not implemented yet
-            return shuffledDeck;
+        
+
+        /// <summary>
+        /// This method resets the deck and then shuffles the deck
+        /// </summary>
+        public void Reset()
+        {
+            _listOfAvalibleCards = new List<Card>();
+            _usedCards = new List<Card>();
+
+            //All 13 cards for spade
+            for (int i = 1; i < 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 's'));
+            }
+            //all 13 cards for club
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'c'));
+            }
+            //all 13 cards for hearts
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'h'));
+            }
+            //all 13 cards for dia
+            for (int i = 1; i <= 14; i++)
+            {
+                _listOfAvalibleCards.Add(new Card(i, 'd'));
+            }
+
+
         }
 
 
         }
+
+    static class MyExtensions
+    {
+        private static Random rng = new Random();
+        /// <summary>
+        /// This is an extension method for the generic list datatype. It will shuffle any (I)List of T objects.  It uses the Fisher-yates shuffle algorithm https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle 
+        /// Explination and implementation guidance was taken from here https://stackoverflow.com/questions/273313/randomize-a-listt from the comment by Eric j https://stackoverflow.com/users/141172/eric-j
+        /// </summary>
+        /// <typeparam name="T"> This method takes a list of T objects</typeparam>
+        /// <param name="list"> </param>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            for (int num = list.Count; num > 1; num--)
+            {
+                int x = rng.Next(num + 1);
+                T val = list[x];
+                list[x] = list[num];
+                list[num] = val;
+            }
+        }
+    }
 
 
     }
